8da58091524c1b24a68dadc088a3a945947db0f3
NicoEcxalibur <nico.zanz@gmail.com>
2021-05-10 19:21:58 +0200

    fixed script and variables

--- app/Views/footer.tpl.php
+++ app/Views/footer.tpl.php
@@ -1,4 +1,5 @@
 <hr>
 <footer>Copyright &copy; 2021</footer>
+<script type="text/javascript" src="<?= $viewData['assetsBaseUri']; ?>script.js"></script>
 </body>
 </html>
--- public/assets/script.js
+++ public/assets/script.js
@@ -1,18 +1,7 @@
-// function toggle(el){
-//     var value = el.options[el.selectedIndex].value,
-//         div = document.querySelector('.passives');
-//     console.log(div)
-//     if (value) {
-//         div.style.display = 'block'
-//     } else {
-        
-//         div.style.display = 'none'
-//     }
-// }
-let div = document.querySelector('.passives');
+var test = document.querySelector('.passives');
 
 function selectInfo(element){
     let msg = element.value
-    div.textContent = msg ;
-    console.log(passive);
+    test.textContent = msg ;
+    console.log(test);
    }
